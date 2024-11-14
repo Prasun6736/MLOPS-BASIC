8806c5275ba34fccc70bab579a30c631ceb7c43f
Prasun Mridha <prasunmridha6@gmail.com>
2024-11-14 12:55:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    description="wine quality",
+    author="prasun",
+    packages=find_packages(),
+    license="MIT",
+)
